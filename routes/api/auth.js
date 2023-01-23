6a4dbadf17d1ff134d62ee9c016e56c429d58584
BorAydin <boraaydins@gmail.com>
2023-01-24 01:08:10 +0300

    User Authentication & Login Route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcryptjs');
 
 const User = require('../../models/User');
 
@@ -13,8 +17,58 @@ router.get('/', auth, async (req, res) => {
     res.json(user);
   } catch(err) {
     console.log(err.message);
-    res.status(500).send('Server Error');
+    res.status(500).send('Sunucu hatası.');
   }
-}) 
+})
+
+// @route  POST api/auth
+// @desc   Authenticate user & get token
+// @access Public
+router.post(
+  '/', 
+  [
+    check('email', 'Geçerli bir mail gereklidir.').isEmail(),
+    check('password', 'Şifre gereklidir.').exists()
+  ], 
+  async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({ errors: errors.array()})
+    }
+
+    const { email, password } = req.body;
+    
+    try {
+      let user = await User.findOne({ email })
+
+      if(!user) {
+        return res.status(400).json({ errors: [{ msg: ' Geçersiz bilgiler.'}]});
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if(!isMatch) {
+        return res.status(400).json({ errors: [{ msg: ' Geçersiz bilgiler.'}]});
+      }
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload, 
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if(err) throw err;
+          res.json({ token });
+        });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Sunucu hatası.')
+    }
+  });
 
 module.exports = router;
