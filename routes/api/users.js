f7dcea2c315fe28eb9be886037911057749b8030
BorAydin <boraaydins@gmail.com>
2023-01-23 17:19:52 +0300

    Request Validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator')
 
-// @route  GET api/users
-// @desc   Test route
+// @route  POST api/users
+// @desc   Register user
 // @access Public
-router.get('/', (req, res) => res.send('User route'))
+router.post(
+  '/', 
+  [
+    check('name', 'İsim gereklidir.').not().isEmpty(),
+    check('email', 'Geçerli bir mail gereklidir.').isEmail(),
+    check('password', 'Şifreniz minumum 8 karakter içermelidir.').isLength({min: 8})
+  ], 
+  (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({ errors: errors.array()})
+    }
+    res.send('User route')
+  });
 
 module.exports = router;
