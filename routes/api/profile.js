9171c9a7d00514e6c7be6122fede19796b229479
BorAydin <boraaydins@gmail.com>
2023-02-21 01:03:51 +0300

    Get Current User Profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
-// @route  GET api/profile
-// @desc   Test route
-// @access Public
-router.get('/', (req, res) => res.send('Profile route'))
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+// @route  GET api/profile/me
+// @desc   Get current users profile
+// @access Private
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: "Bu kullanıcı için profil yok." });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Sunucu Hatası");
+  }
+});
 
 module.exports = router;
