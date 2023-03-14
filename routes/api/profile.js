7baf2e006aef692ff8a55fffc190b1a467249583
BorAydin <boraaydins@gmail.com>
2023-03-14 06:52:21 +0300

    Get Github Repos For Profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const request = require("request");
+const config = require("config");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 const { check, validationResult } = require("express-validator");
 
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
+const { response } = require("express");
 
 // @route  GET api/profile/me
 // @desc   Get current users profile
@@ -295,6 +298,36 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
     await profile.save();
 
     res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Sunucu Hatası");
+  }
+});
+
+// @route  GET api/profile/github/:username
+// @desc   Get user repos from Github
+// @access Public
+router.get("/github/:username", (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        "githubClientId"
+      )}&client_secret=${config.get("githubSecret")}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.log(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: "Github profili bulunamadı." });
+      }
+
+      res.json(JSON.parse(body));
+    });
   } catch (err) {
     console.log(err.message);
     res.status(500).send("Sunucu Hatası");
