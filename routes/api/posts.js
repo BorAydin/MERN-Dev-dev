4aa59b4916e5b0b078e56e27103c8098d3b4a0ba
BorAydin <boraaydins@gmail.com>
2023-03-16 01:58:48 +0300

    Add Post Route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,43 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
+const auth = require("../../middleware/auth");
 
-// @route  GET api/posts
-// @desc   Test route
-// @access Public
-router.get('/', (req, res) => res.send('Posts route'))
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+// @route  POST api/posts
+// @desc   Create a post
+// @access Private
+router.post(
+  "/",
+  auth,
+  [check("text", "Yorum gereklidir.").not().isEmpty()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Sunucu hatası.");
+    }
+  }
+);
 
 module.exports = router;
