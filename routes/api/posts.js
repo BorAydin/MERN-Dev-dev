2adcc13bdd358dc0e2589e5015f4c2498daf63fa
BorAydin <boraaydins@gmail.com>
2023-03-16 02:49:59 +0300

    Get & Delete Post Routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -40,4 +40,65 @@ router.post(
   }
 );
 
+// @route  GET api/posts
+// @desc   Get all posts
+// @access Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Sunucu hatası.");
+  }
+});
+
+// @route  GET api/posts/:id
+// @desc   Get post by ID
+// @access Private
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post bulunamadı." });
+    }
+    res.json(post);
+  } catch (err) {
+    console.log(err);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post bulunamadı." });
+    }
+    res.status(500).send("Sunucu hatası.");
+  }
+});
+
+// @route  DELETE api/posts/:id
+// @desc   Delete a post
+// @access Private
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post bulunamadı." });
+    }
+
+    // Check user
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Kullanıcı yetkili değil." });
+    }
+
+    await post.remove();
+
+    res.json({ msg: "Post kaldırıldı." });
+  } catch (err) {
+    console.log(err);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post bulunamadı." });
+    }
+    res.status(500).send("Sunucu hatası.");
+  }
+});
+
 module.exports = router;
