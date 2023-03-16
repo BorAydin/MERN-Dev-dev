84ca7bd0a5f9e1c8f711692dc7013800226074eb
BorAydin <boraaydins@gmail.com>
2023-03-16 04:40:35 +0300

    Add & Remove Comment Routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,7 +4,6 @@ const { check, validationResult } = require("express-validator/check");
 const auth = require("../../middleware/auth");
 
 const Post = require("../../models/Post");
-const Profile = require("../../models/Profile");
 const User = require("../../models/User");
 
 // @route  POST api/posts
@@ -152,6 +151,79 @@ router.put("/unlike/:id", auth, async (req, res) => {
     await post.save();
 
     res.json(post.likes);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Sunucu hatası.");
+  }
+});
+
+// @route  POST api/posts/comment/:id
+// @desc   Comment on a post
+// @access Private
+router.post(
+  "/comment/:id",
+  auth,
+  [check("text", "Yorum gereklidir.").not().isEmpty()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const post = await Post.findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+
+      post.comments.unshift(newComment);
+
+      await post.save();
+
+      res.json(post.comments);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Sunucu hatası.");
+    }
+  }
+);
+
+// @route  DELETE api/posts/comment/:id/:comment_id
+// @desc   Delete comment
+// @access Private
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Pull out comment
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+
+    // Make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: "Yorum yok." });
+    }
+
+    // Check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Kullanıcı yetkili değil." });
+    }
+
+    const removeIndex = post.comments
+      .map((comment) => comment.user.toString())
+      .indexOf(req.user.id);
+
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.comments);
   } catch (err) {
     console.log(err);
     res.status(500).send("Sunucu hatası.");
