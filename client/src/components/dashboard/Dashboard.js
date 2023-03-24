5fca702c0cb23cd6af56c0480cbc6dbc089765ba
BorAydin <boraaydins@gmail.com>
2023-03-24 05:06:54 +0300

    Starting on The Dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,13 +3,40 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getCurrentProfile } from "../../actions/profile";
 import profile from "../../reducers/profile";
+import Spinner from "../layout/Spinner";
+import { Link } from "react-router-dom";
 
-const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+const Dashboard = ({
+  getCurrentProfile,
+  auth: { user },
+  profile: { profile, loading },
+}) => {
   useEffect(() => {
     getCurrentProfile();
   }, []);
 
-  return <div>Dashboard</div>;
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <>
+      <div className="container">
+        <h1 className="large text-primary">Gösterge Paneli</h1>
+        <p className="lead">
+          <i className="fas fa-user"></i> Hoşgeldin {user && user.name}
+        </p>
+        {profile !== null ? (
+          <>Hey</>
+        ) : (
+          <>
+            <p>Profil oluşturmamışsın. Lütfen oluştur.</p>
+            <Link to="create-profile" className="btn btn-primary my-1">
+              Profil Oluşturma
+            </Link>
+          </>
+        )}
+      </div>
+    </>
+  );
 };
 
 Dashboard.propTypes = {
