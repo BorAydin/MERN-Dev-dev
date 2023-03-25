640f08946d07b87206922792621732e7789716e0
BorAydin <boraaydins@gmail.com>
2023-03-25 11:47:17 +0300

    Create Profile Action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -19,3 +19,39 @@ export const getCurrentProfile = () => async (dispatch) => {
     });
   }
 };
+
+// Create or update profile
+export const createProfile =
+  (formData, edit = false) =>
+  async (dispatch) => {
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      const res = await axios.post("/api/profile", formData, config);
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data,
+      });
+
+      dispatch(
+        setAlert(
+          edit ? "Profil güncellendi." : "Profil oluşturuldu.",
+          "success"
+        )
+      );
+    } catch (err) {
+      const errors = err.response.data.errors;
+
+      if (errors) {
+        errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+      }
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.StatusText, status: err.response.status },
+      });
+    }
+  };
--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
+import { createProfile } from "../../actions/profile";
 
-const CreateProfile = (props) => {
+const CreateProfile = ({ createProfile }) => {
   const [formData, setFormData] = useState({
     company: "",
     website: "",
@@ -39,6 +40,11 @@ const CreateProfile = (props) => {
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    createProfile(formData);
+  };
+
   return (
     <>
       <section className="container">
@@ -48,7 +54,7 @@ const CreateProfile = (props) => {
           ekle.
         </p>
         <small>* = Doldurulması zorunlu alanlar.</small>
-        <form className="form">
+        <form className="form" onSubmit={(e) => onSubmit(e)}>
           <div className="form-group">
             <select name="status" value={status} onChange={(e) => onChange(e)}>
               <option value="0">* Durumunuzu Seçin.</option>
@@ -198,6 +204,8 @@ const CreateProfile = (props) => {
   );
 };
 
-CreateProfile.propTypes = {};
+CreateProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired,
+};
 
-export default CreateProfile;
+export default connect(null, { createProfile })(CreateProfile);
