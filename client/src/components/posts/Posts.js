e9cf50e0d4ecfb42d44cd2402f9fefb7300d72c8
BorAydin <boraaydins@gmail.com>
2023-04-16 22:01:36 +0300

    Post Item Component

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,13 +2,28 @@ import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
+import PostItem from "./PostItem";
 import { getPosts } from "../../actions/post";
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
-  return <div></div>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <section className="container">
+      <h1 className="large text-primary">Gönderiler</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Topluluğa hoş geldin.
+      </p>
+      <div className="posts">
+        {posts.map((post) => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 Posts.propTypes = {
