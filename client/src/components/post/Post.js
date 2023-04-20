b9443d9712abdefe37586f0a184e03dbcc8eafba
BorAydin <boraaydins@gmail.com>
2023-04-20 03:10:41 +0300

    Adding Comments

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,6 +5,7 @@ import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import PostItem from "../posts/PostItem";
 import { getPost } from "../../actions/post";
+import CommentForm from "./CommentForm";
 
 const Post = ({ getPost, post: { post, loading } }) => {
   const { id } = useParams();
@@ -19,6 +20,7 @@ const Post = ({ getPost, post: { post, loading } }) => {
         Gönderiler
       </Link>
       <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
     </section>
   );
 };
